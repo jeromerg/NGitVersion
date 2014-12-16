98c9e30886cd04496ee6b56894ec62b62556eaa6
Jerome <jeromerg@gmx.net>
2014-12-16 07:58:28 +0100

    temporary state

--- src/NGitVersion/NGitVersion.cs
+++ src/NGitVersion/NGitVersion.cs
@@ -1,99 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
+﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Antlr4.StringTemplate;
-using NLog;
-using Version.Template;
-using Version.Templates;
-using Version.Util.Git;
+using LibGit2Sharp;
 
-// ReSharper disable UnusedVariable
-// ReSharper disable ConditionIsAlwaysTrueOrFalse
-// ReSharper disable HeuristicUnreachableCode
-#pragma warning disable 162
-
-namespace Version {
+namespace NGitVersion {
     public static class NGitVersion {
-        private static readonly Logger sLogger = LogManager.GetCurrentClassLogger();
-        
-        // dir relative to output project directory ${ProjectDir}\bin
+
+        // directories are relative to output project directory ${ProjectDir}\bin
         private const string TEMPLATE_DIR = @"..\Templates\";
-        private const string TEMPLATE_FILES = @"*.*.template";
-        private const string TEMPLATE_FILES_STRING_TO_REMOVE = @".template";
-        // dir relative to output project directory ${ProjectDir}\bin
+        private const string TEMPLATE_FILE_PREFIX = @"File_";
         private const string OUTPUT_DIR = @"..\Result\";
-        private const string OUTPUT_FILES_PATTERN = @"{0}.generated{1}";
 
-        public static void Main(string [] args) {
-            
-            var gitExe = new GitRepository();
-            GitLog gitLog = gitExe.Log;
-            int revision = gitLog.Commits.Count;
-            string lastCommitHash = gitLog.Commits[0].Hash;
-            string lastCommitShortHash = gitLog.Commits[0].ShortHash;
-            string oldestCommitHash = gitLog.Commits[gitLog.Commits.Count-1].Hash;
-            
-            // ASSERT OLDEST COMMIT IF DEFINED
-            bool isRepositoryMismatch = CheckRepositoryHistory(oldestCommitHash, ref revision);
-            
-            // Substitution key-values
-            SharedTemplateInfo.AdditionalAttributes.Add("<revision>", revision.ToString(CultureInfo.InvariantCulture));
-            SharedTemplateInfo.AdditionalAttributes.Add("<shortHash>", lastCommitShortHash);
-            SharedTemplateInfo.AdditionalAttributes.Add("<localChangeInfo>", (hasLocalChange ? "(with local change)" : "") + 
-                                                (isRepositoryMismatch ? "(repository/revision mismatch)" : ""));
+        public static void Main(string [] args)
+        {
+            //git rev-list HEAD --count
+            var builtInVars = new Dictionary<string, object> { { "git", new LibGit2Sharp.Repository(".") } };
 
-            // Template
-            string[] templateFiles = Directory.GetFiles(TEMPLATE_DIR, TEMPLATE_FILES);
-            foreach (var templateFile in templateFiles) {
-                ProcessTemplate(templateFile);
-            }
+            foreach (var templateFile in Directory.GetFiles(TEMPLATE_DIR, TEMPLATE_FILE_PREFIX + "*.stg"))
+                ProcessTemplate(templateFile, builtInVars);
         }
 
-        private static void ProcessTemplate(string templateFile)
+        private static void ProcessTemplate(string templateFile, Dictionary<string, object> builtInVars)
         {
-            sLogger.Info("Processing template file: " + templateFile);
+            Template template = new TemplateGroupFile(templateFile)
+                                        .GetInstanceOf(Path.GetFileNameWithoutExtension(templateFile));
 
+            foreach (var v in builtInVars)
+                template.Add(v.Key, v.Value);
 
-            string fileContent = File.ReadAllText(templateFile);
-            var template = new Template(fileContent);
-            foreach (var substitute in SharedTemplateInfo.AdditionalAttributes)
-            {
-                sLogger.Info("... substituting {0} by {1}", substitute.Key, substitute.Value);
-                fileContent = fileContent.Replace(substitute.Key, substitute.Value.ToString());
-            }
-
-            var targetFile = BuildTargetFile(templateFile);
-
-            sLogger.Info("Exporting result into file: " + targetFile);
-            File.WriteAllText(targetFile, fileContent);
-        }
-
-        private static bool CheckRepositoryHistory(string oldestCommitHash, ref int revision)
-        {
-            if (SharedTemplateInfo.EXPECTED_OLDEST_COMMIT_HASH == null)
-                return false;
-
-            bool isRepositoryMismatch = oldestCommitHash != SharedTemplateInfo.EXPECTED_OLDEST_COMMIT_HASH;
-            if (isRepositoryMismatch)
-            {
-                revision = Int16.MaxValue;
-                sLogger.Warn("Oldest revision has id {0}, but is expected to be {1}. Local git repository is not the original. Revision is set to {2}", oldestCommitHash, SharedTemplateInfo.EXPECTED_OLDEST_COMMIT_HASH, Int16.MaxValue);
-            }
-            else
-            {
-                sLogger.Info(string.Format("Oldest revision has id {0} as expected", SharedTemplateInfo.EXPECTED_OLDEST_COMMIT_HASH));
-            }
-            return isRepositoryMismatch;
+            File.WriteAllText(BuildTargetFile(templateFile), template.Render());
         }
 
         private static string BuildTargetFile(string templateFile)
         {
-            string targetFile = templateFile.Replace(TEMPLATE_FILES_STRING_TO_REMOVE, "");
-            targetFile = OUTPUT_DIR + string.Format(OUTPUT_FILES_PATTERN,
-                Path.GetFileNameWithoutExtension(targetFile),
-                Path.GetExtension(targetFile));
-            return targetFile;
+            string outputFileName
+                = Path.GetFileNameWithoutExtension(templateFile)
+                      .Replace(TEMPLATE_FILE_PREFIX, "")
+                      .Replace("_", ".");
+
+            return OUTPUT_DIR + outputFileName;
         }
     }
 }
